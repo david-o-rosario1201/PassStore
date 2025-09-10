69afb7965bad5aec54d711c2a71de568470c1de4
david-o-rosario1201 <finnnn023@gmail.com>
2025-09-10 10:16:02 -0400

    Crea la interface SubcuentaUiEvent

---
+++ app/src/main/java/edu/ucne/passstore/presentation/subcuenta/SubcuentaUiEvent.kt
@@ -0,0 +1,9 @@
+package edu.ucne.passstore.presentation.subcuenta
+
+sealed interface SubcuentaUiEvent {
+    data class NombreChanged(val nombre: String): SubcuentaUiEvent
+    data class PasswordChanged(val password: String): SubcuentaUiEvent
+    data class CuentaIdChanged(val cuentaId: Int): SubcuentaUiEvent
+    data object Save: SubcuentaUiEvent
+    data object Delete: SubcuentaUiEvent
+}
