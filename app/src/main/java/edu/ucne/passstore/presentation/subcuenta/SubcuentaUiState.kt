e3477a0c9b9a5a18c7ad66a07c905c7c7189a1d0
david-o-rosario1201 <finnnn023@gmail.com>
2025-09-10 10:16:28 -0400

    Crea la data class SubcuentaUiState

---
+++ app/src/main/java/edu/ucne/passstore/presentation/subcuenta/SubcuentaUiState.kt
@@ -0,0 +1,17 @@
+package edu.ucne.passstore.presentation.subcuenta
+
+import edu.ucne.passstore.data.local.entities.CuentaEntity
+import edu.ucne.passstore.data.local.entities.SubcuentaEntity
+
+data class SubcuentaUiState(
+    val subcuentaId: Int? = null,
+    val nombreUsuario: String = "",
+    val password: String = "",
+    val cuentaId: Int = 0,
+    val cuentas: List<CuentaEntity> = emptyList(),
+    val subcuentas: List<SubcuentaEntity> = emptyList(),
+    val errorNombreUsuario: String = "",
+    val errorPassword: String = "",
+    val errorCuentaId: String = "",
+    val success: Boolean = false
+)
