0823b190d6d273e175ce70e68b2e33f615724210
david-o-rosario1201 <finnnn023@gmail.com>
2025-09-26 21:46:20 -0400

    Crea el archivo AppTheme

---
+++ app/src/main/java/edu/ucne/passstore/presentation/components/AppTheme.kt
@@ -0,0 +1,39 @@
+package edu.ucne.passstore.presentation.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun AppTheme(
+    isDarkMode: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+){
+    val LightColors = lightColorScheme(
+        primary = Color(0xFF0D47A1),
+        onPrimary = Color.White,
+        background = Color.White,
+        onBackground = Color.Black,
+        surface = Color.White,
+        onSurface = Color.Black,
+    )
+
+    val DarkColors = darkColorScheme(
+        primary = Color(0xFF90CAF9),
+        onPrimary = Color.Black,
+        background = Color.Black,
+        onBackground = Color.White,
+        surface = Color(0xFF121212),
+        onSurface = Color.White,
+    )
+
+    val colors = if (isDarkMode) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
