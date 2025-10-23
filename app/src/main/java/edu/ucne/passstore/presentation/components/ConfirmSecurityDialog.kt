dd3a64a28f399ffc8d7385b062b9160c4918e923
david-o-rosario1201 <finnnn023@gmail.com>
2025-10-23 10:52:24 -0400

    Crea un modal ConfirmSecurityDialog para confirmar que se quiere cambiar el código de seguridad

---
+++ app/src/main/java/edu/ucne/passstore/presentation/components/ConfirmSecurityDialog.kt
@@ -0,0 +1,137 @@
+package edu.ucne.passstore.presentation.components
+
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import edu.ucne.passstore.R
+import edu.ucne.passstore.presentation.settings.SettingUiEvent
+
+@Composable
+fun ConfirmSecurityCode(
+    title: String,
+    context: Context,
+    onEvent: (SettingUiEvent) -> Unit
+){
+    Dialog(onDismissRequest = { onEvent(SettingUiEvent.DismissAllModals)}) {
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        ){
+            Column(
+                modifier = Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                //Icono
+                Image(
+                    painter = painterResource(R.drawable.blindaje),
+                    contentDescription = null,
+                    modifier = Modifier.size(80.dp)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                //Titulo
+                Text(
+                   text = title,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 22.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Divider(modifier = Modifier.padding(vertical = 8.dp))
+
+                //Pregunta
+                Text(
+                    text = context.getString(R.string.confirm_modal_question),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                //otra
+                Text(
+                    buildAnnotatedString {
+                        append(context.getString(R.string.confirm_push))
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                            append(context.getString(R.string.confirm_accept))
+                        }
+                        append(context.getString(R.string.confirm_continue))
+                    },
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                //Botones
+                Row {
+                    Button(
+                        onClick = { onEvent(SettingUiEvent.DismissAllModals)},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.error,
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        ),
+                        modifier = Modifier.width(140.dp)
+                    ) {
+                        Text(
+                            text = context.getString(R.string.cancel_button),
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(10.dp))
+
+                    Button(
+                        onClick = { onEvent(SettingUiEvent.Save)},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        ),
+                        modifier = Modifier.width(140.dp)
+                    ) {
+                        Text(
+                            text = context.getString(R.string.accept_button),
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
