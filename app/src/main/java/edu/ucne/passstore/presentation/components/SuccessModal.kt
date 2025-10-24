d7d6a9e6689f75b620a6480dfb2d2b93d3cfc924
david-o-rosario1201 <finnnn023@gmail.com>
2025-10-24 15:51:26 -0400

    Crea al modal SuccessModal

---
+++ app/src/main/java/edu/ucne/passstore/presentation/components/SuccessModal.kt
@@ -0,0 +1,67 @@
+package edu.ucne.passstore.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import edu.ucne.passstore.R
+import edu.ucne.passstore.presentation.settings.SettingUiEvent
+import kotlinx.coroutines.delay
+
+@Composable
+fun SuccessModal(
+    title: String,
+    durationMillis: Long = 2000,
+    onEvent: (SettingUiEvent) -> Unit
+){
+    Dialog(onDismissRequest = {}) {
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        ){
+            Column(
+                modifier = Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                //Titulo
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 22.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                //Icono
+                Image(
+                    painter = painterResource(R.drawable.comprobado),
+                    contentDescription = null,
+                    modifier = Modifier.size(80.dp)
+                )
+            }
+        }
+    }
+
+    LaunchedEffect(Unit){
+        delay(durationMillis)
+        onEvent(SettingUiEvent.ShowSuccessModal(false))
+    }
+}
