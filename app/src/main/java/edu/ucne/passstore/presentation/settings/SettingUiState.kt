a8cf693287e20f4fadf48907a0802f8c1931e3d1
david-o-rosario1201 <finnnn023@gmail.com>
2025-11-11 12:29:38 -0400

    Cambia el nombre de la variable showConfirmSecurityCode a showConfirmModal y crea una nueva llamada codeSucceeded

--- app/src/main/java/edu/ucne/passstore/presentation/settings/SettingUiState.kt
+++ app/src/main/java/edu/ucne/passstore/presentation/settings/SettingUiState.kt
@@ -4,12 +4,13 @@ data class SettingUiState(
     val pinCode: String = "",
     val showSecurityCode: Boolean = false,
     val showNewSecurityCode: Boolean = false,
-    val showConfirmSecurityCode: Boolean = false,
+    val showConfirmModal: Boolean = false,
     val checkCodeIsNotEmpty: Boolean = false,
     val checkCodeIsIncorrect: Boolean = false,
     val showCodesAreNotEqualsMessage: Boolean = false,
     val showEmptyMessage: Boolean = false,
     val showIncompleteMessage:Boolean = false,
     val showSuccessModal: Boolean = false,
-    val errorState: Boolean = false
+    val errorState: Boolean = false,
+    val codeSucceeded: Boolean = false
 )
