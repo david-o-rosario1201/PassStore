7ca5d54abfe839fb10d8edf4e704f696604ec6e4
david-o-rosario1201 <finnnn023@gmail.com>
2025-11-11 12:19:55 -0400

    Agrega la anotación @RequiresApi(Build.VERSION_CODES.TIRAMISU), agrega la función para ir hacia la vista de las subcuentas, agrega el composable de VieSubcuentaScreen

--- app/src/main/java/edu/ucne/passstore/presentation/navigation/PassStoreNavHost.kt
+++ app/src/main/java/edu/ucne/passstore/presentation/navigation/PassStoreNavHost.kt
@@ -1,14 +1,19 @@
 package edu.ucne.passstore.presentation.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import edu.ucne.passstore.presentation.home.HomeScreen
 import edu.ucne.passstore.presentation.settings.SettingScreen
 import edu.ucne.passstore.presentation.subcuenta.SubcuentaScreen
+import edu.ucne.passstore.presentation.subcuenta.ViewSubcuentaScreen
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun PassStoreNavHost(
     navHostController: NavHostController
@@ -21,6 +26,9 @@ fun PassStoreNavHost(
         composable<Screen.HomeScreen> {
             HomeScreen(
                 context = context,
+                goViewSubcuentaScreen = { cuentaId ->
+                    navHostController.navigate(Screen.ViewSubcuentaScreen(cuentaId))
+                },
                 navHostController = navHostController
             )
         }
@@ -34,6 +42,17 @@ fun PassStoreNavHost(
             SettingScreen(
                 context = context,
                 navHostController = navHostController
+            )
+        }
+        composable<Screen.ViewSubcuentaScreen> {id ->
+            val cuentaId = id.toRoute<Screen.ViewSubcuentaScreen>().cuentaId
+
+            ViewSubcuentaScreen(
+                cuentaId = cuentaId,
+                context = context,
+                goBack = {
+                    navHostController.navigate(Screen.HomeScreen)
+                }
             )
         }
     }
