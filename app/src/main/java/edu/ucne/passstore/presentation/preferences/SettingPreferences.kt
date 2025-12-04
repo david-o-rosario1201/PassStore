e35ac1d40756cdfa164d4ed2fa19179332b5d15b
david-o-rosario1201 <finnnn023@gmail.com>
2025-12-04 16:18:38 -0400

    Agrega una llave llamada BIOMETRIC_AUTH y agregalo a la función de datos por default, agrega las funciones set y get BiometricAuth

--- app/src/main/java/edu/ucne/passstore/presentation/preferences/SettingPreferences.kt
+++ app/src/main/java/edu/ucne/passstore/presentation/preferences/SettingPreferences.kt
@@ -1,6 +1,7 @@
 package edu.ucne.passstore.presentation.preferences
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
@@ -13,6 +14,7 @@ class SettingPreferences @Inject constructor(
 ){
     companion object{
         val PIN_CODE = stringPreferencesKey("pin_code")
+        val BIOMETRIC_AUTH = booleanPreferencesKey("biometric_auth")
         const val DEFAULT_CODE = "123456"
     }
 
@@ -20,6 +22,7 @@ class SettingPreferences @Inject constructor(
         context.dataStore.edit { pref ->
             if(!pref.contains(PIN_CODE)){
                 pref[PIN_CODE] = DEFAULT_CODE
+                pref[BIOMETRIC_AUTH] = false
             }
         }
     }
@@ -29,9 +32,20 @@ class SettingPreferences @Inject constructor(
         return preferences[PIN_CODE] ?: ""
     }
 
+    suspend fun getBiometricAuth(): Boolean{
+        val preferences = context.dataStore.data.first()
+        return preferences[BIOMETRIC_AUTH] ?: false
+    }
+
     suspend fun setPinCode(pinCode: String){
         context.dataStore.edit { preferences ->
             preferences[PIN_CODE] = pinCode
+        }
+    }
+
+    suspend fun setBiometricAuth(biometricAuth: Boolean){
+        context.dataStore.edit { preferences ->
+            preferences[BIOMETRIC_AUTH] = biometricAuth
         }
     }
 }
