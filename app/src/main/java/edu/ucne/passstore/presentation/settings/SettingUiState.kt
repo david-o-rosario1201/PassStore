cb6159250d0d909e126e016846d607c34a4837f3
david-o-rosario1201 <finnnn023@gmail.com>
2025-12-04 16:25:43 -0400

    Crea las variables biometricAuth, requestBiometricAuth, showBiometricModal y showBiometricAuthDisabledModal

--- app/src/main/java/edu/ucne/passstore/presentation/settings/SettingUiState.kt
+++ app/src/main/java/edu/ucne/passstore/presentation/settings/SettingUiState.kt
@@ -4,6 +4,8 @@ data class SettingUiState(
     val pinCode: String = "",
     val userName: String = "",
     val userDateRegister: String = "",
+    val biometricAuth: Boolean = false,
+    val requestBiometricAuth: Boolean = false,
     val showSecurityCode: Boolean = false,
     val showNewSecurityCode: Boolean = false,
     val showUserView: Boolean = false,
@@ -14,6 +16,8 @@ data class SettingUiState(
     val showEmptyMessage: Boolean = false,
     val showIncompleteMessage:Boolean = false,
     val showSuccessModal: Boolean = false,
+    val showBiometricModal: Boolean = false,
+    val showBiometricAuthDisabledModal: Boolean = false,
     val errorState: Boolean = false,
     val codeSucceeded: Boolean = false
 )
